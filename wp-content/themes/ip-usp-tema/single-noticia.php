5b975aff793ae7e61b3df4b863890c6a8cef9a97
lhas <luizhrqas@gmail.com>
2016-08-07 15:16:58 -0300

    resolved #1: agora existe um modelo para notícia e o modelo padrão de página está igual o sugerido

---
+++ wp-content/themes/ip-usp-tema/single-noticia.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Noticia template file.
+ *
+ * @package unite
+ */
+
+get_header(); ?>
+
+	<div class="container conteudo">
+		
+		<?php get_template_part( 'content', 'single-noticia' ); ?>
+			
+
+	</div><!--  container  -->
+
+
+<?php get_footer(); ?>
