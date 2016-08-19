bc31c1d37b90fe27c208a34c5f4394e36564758a
lhas <luizhrqas@gmail.com>
2016-08-19 08:59:45 -0300

    atualizacao

--- wp-content/themes/ip-usp-tema/functions.php
+++ wp-content/themes/ip-usp-tema/functions.php
@@ -140,7 +140,8 @@ function set_custom_bg($page_id = null){
 		$page_id = get_queried_object_id();
 	}
 
-	if ( has_post_thumbnail( $page_id ) ) :
+
+	if ( has_post_thumbnail( $page_id ) && $page_id > 0 ) :
 	    $image_array = wp_get_attachment_image_src( get_post_thumbnail_id( $page_id ), 'full' );
 	    $image = $image_array[0];
 	else :
